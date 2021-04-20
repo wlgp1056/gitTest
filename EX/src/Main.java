a5d0708f85229db555c2676ac5153e1bec345e8f
박지혜 <@DESKTOP-BHQIN0E>
2021-04-20 15:26:44 +0900

    새롭게 프로젝트를 시작합니다. 아자아자

---
+++ EX/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a,b;
+		
+		System.out.print("a를 입력하세요 : ");
+		a = sc.nextInt();
+		System.out.print("b를 입력하세요 : ");
+		b = sc.nextInt();
+		
+		System.out.println("결과는 : "+ (a+b));
+	}
+		
+		
+	}
+
